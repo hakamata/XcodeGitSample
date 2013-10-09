2fadca16cda72e85c237e0b25b27678308505483
hakamata <hakamata@hakamata.local>
2013-10-09 22:44:31 +0900

    Initial Commit

---
+++ XcodeGitSample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  XcodeGitSample
+//
+//  Created by hakamata on 2013/10/09.
+//  Copyright (c) 2013年 hakamata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
